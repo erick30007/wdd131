9ec1c0e013349d0d412dfabfb9517ac456635b0c
erick3000 <erickelfuerte987@gmail.com>
2024-10-19 11:20:05 -0500

    update

--- scripts/demo-proyect.js
+++ scripts/demo-proyect.js
@@ -1,1 +1,8 @@
+const yearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("lastmodified").textContent = "Last modified: " + lastModified;
+
 console.log("Site plan loaded successfully.");
